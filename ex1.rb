34826c7b33dbb60640eb865f38f78f53739709fd
jwlindsey <j.w.lindsey1986@gmail.com>
2017-01-09 12:25:35 -0600

    practice update

--- ex1.rb
+++ ex1.rb
@@ -1,12 +1,21 @@
-print "Give me a number: "
-number = gets.chomp.to_i
+user_name = ARGV.first
+prompt = 'peaches '
 
-bigger = number * 100
-puts "A bigger number is #{bigger}."
+puts "Hi #{user_name}."
+puts "I'd like to ask you a few questions."
+puts "Do you like me #{user_name}? "
+puts prompt
+likes = $stdin.gets.chomp
 
-print "Give me another number: "
-another = gets.chomp
-number = another.to_i
+puts "Where do you live #{user_name}? "
+puts prompt
+lives = $stdin.gets.chomp
 
-smaller = number / 100
-puts "A smaller number is #{smaller}."
+puts "What kind of computer do you have? ", prompt
+computer = $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}. Not sure where that is.
+And you have a #{computer}. nice.
+"""
--- hello.rb
+++ hello.rb
@@ -2,5 +2,5 @@
 #This program follows the instructions from the
 #Firehose Intro course at the Firehose Project.
 
-puts "Today, I wrote code"
-puts "and I am a coding machine..."
+#puts "Today, I wrote code"
+#puts "and I am a coding machine..."
