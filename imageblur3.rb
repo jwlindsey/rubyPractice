f2946b4a7217076f2c3f4de378ffacf2c870f698
jwlindsey <j.w.lindsey1986@gmail.com>
2017-02-06 20:04:46 -0600

    immageblur3 manhattan distance finished

--- imageblur3.rb
+++ imageblur3.rb
@@ -11,24 +11,24 @@ class Image
     end
   end
 
-  def blur
-    #Create a clone of image
-    @new_image = Marshal.load(Marshal.dump(@array))
-    #Loop through array, find the 1 pixel
-    @array.each_with_index do |row, row_index|
-      row.each_with_index do |pixel, col_index |
-        if pixel == 1
-          #Blur the pixel with limits of where to stop
-          @new_image[row_index - 1][col_index] = 1 unless row_index == 0
+  def blur(distance=1)
+    distance.times do
+        #Create a clone of image
+      @new_image = Marshal.load(Marshal.dump(@array))
+        #Loop through array, find the 1 pixel
+      @array.each_with_index do |row, row_index|
+        row.each_with_index do |pixel, col_index |
+          if pixel == 1
+            #Blur the pixel with limits of where to stop
+            @new_image[row_index - 1][col_index] = 1 unless row_index == 0
+            @new_image[row_index + 1][col_index] = 1 unless (row_index + 1) >= @array.length
+            @new_image[row_index][col_index - 1] = 1 unless col_index == 0
+            @new_image[row_index][col_index + 1] = 1 unless (col_index + 1) >= row.length
 
-          @new_image[row_index + 1][col_index] = 1 unless (row_index + 1) >= @array.length
-
-          @new_image[row_index][col_index - 1] = 1 unless col_index == 0
-
-          @new_image[row_index][col_index + 1] = 1 unless (col_index + 1) >= row.length
-
+          end
         end
       end
+      @array = @new_image
     end
     #clones image and edits the clone
     Image.new(@new_image)
@@ -51,5 +51,5 @@ afterImage = Image.new([
 ])
 
 
-new_image = image.blur
+new_image = image.blur(2)
 new_image.output_image
